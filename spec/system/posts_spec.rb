465bc66a878b02d5894c3d60f325a40c525ffabd
taji <taji@sonicgarden.jp>
2024-05-02 08:59:00 +0900

    ポストの編集と削除テストも追加

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -27,5 +27,28 @@ RSpec.describe 'ポスト機能', type: :system do
       end.to change(Post, :count).by(1)
       expect(page).to have_content 'fuga'
     end
+
+    it 'ポストを編集できること' do
+      post = create(:post, contents: 'foobar')
+      visit post_path(post)
+      expect(page).to have_content 'foobar'
+      click_on '編集'
+      fill_in '内容', with: 'piyo'
+      click_on '更新する'
+      expect(page).to have_content '投稿を編集しました。'
+      expect(page).not_to have_content 'foobar'
+      expect(page).to have_content 'piyo'
+    end
+
+    it 'ポストを削除できること' do
+      post = create(:post, contents: 'MudaMuda')
+      visit post_path(post)
+      expect(page).to have_content 'MudaMuda'
+      expect do
+        click_on '削除'
+        expect(page).to have_content '投稿を削除しました。'
+      end.to change(Post, :count).by(-1)
+      expect(page).not_to have_content 'MudaMuda'
+    end
   end
 end
