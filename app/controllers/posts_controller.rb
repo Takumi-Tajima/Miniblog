d0b15bff5ee68f10741a9a4472c34a383a8c3a80
taji <taji@sonicgarden.jp>
2024-05-09 11:13:49 +0900

    posts_controllerを分割する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show edit update destroy]
+  before_action :set_post, only: %i[show]
   skip_before_action :authenticate_user!, only: %i[index show]
-  before_action :ensure_ownership, only: %i[edit update destroy]
 
   def index
     @posts = Post.default_order
@@ -9,48 +8,9 @@ class PostsController < ApplicationController
 
   def show; end
 
-  def new
-    @post = current_user.posts.build
-  end
-
-  def edit; end
-
-  def create
-    @post = current_user.posts.build(post_params)
-
-    if @post.save
-      redirect_to posts_path, notice: t('controllers.common.created', model: '投稿')
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @post.update(post_params)
-      redirect_to post_path(@post), notice: t('controllers.common.updated', model: '投稿'), status: :see_other
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @post.destroy!
-    redirect_to posts_path, notice: t('controllers.common.destroyed', model: '投稿'), status: :see_other
-  end
-
   private
 
   def set_post
     @post = Post.find(params[:id])
-  end
-
-  def ensure_ownership
-    unless @post.user == current_user
-      redirect_to root_path
-    end
-  end
-
-  def post_params
-    params.require(:post).permit(:content)
   end
 end
