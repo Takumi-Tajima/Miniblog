2d00206357f3a8d3ecfdecccaba6f5a3bd9e1899
taji <taji@sonicgarden.jp>
2024-05-09 11:37:03 +0900

    ルーティング：updateのタイポを修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   scope module: :users do
-    resources :posts, only: %i[new create edit updatae destroy]
+    resources :posts, only: %i[new create edit update destroy]
   end
   resources :posts, only: %i[index show]
 end
