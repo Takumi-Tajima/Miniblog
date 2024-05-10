aaae83342049ccce0ddd108eb3e8ed3bfa0027e2
taji <taji@sonicgarden.jp>
2024-05-10 11:02:12 +0900

    Userにフォローとフォロワーのアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+  has_many :relationships, foreign_key: 'following_id', dependent: :destroy
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followings, through: :relationships, source: :follower
+  has_many :followers, through: :reverse_of_relationships, source: :following
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/ }, length: { maximum: 20 }
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).*\z/ }
 end
