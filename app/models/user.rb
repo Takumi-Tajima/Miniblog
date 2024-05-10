b0a6bca197f11a1bfea7130793effe4443934bea
taji <taji@sonicgarden.jp>
2024-05-10 11:39:09 +0900

    Userモデルにinverse_ofを指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  has_many :relationships, foreign_key: 'following_id', dependent: :destroy
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :relationships, foreign_key: 'following_id', dependent: :destroy, inverse_of: 'following'
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: 'follwers'
   has_many :followings, through: :relationships, source: :follower
   has_many :followers, through: :reverse_of_relationships, source: :following
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/ }, length: { maximum: 20 }
