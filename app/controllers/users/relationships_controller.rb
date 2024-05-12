c300e4b0d755a6cf599492574111c257d4baf437
taji <taji@sonicgarden.jp>
2024-05-13 08:40:15 +0900

    relationships_controller：タイポ修正

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -4,7 +4,7 @@ class Users::RelationshipsController < ApplicationController
     redirect_to request.referer || root_path
   end
 
-  def destory
+  def destroy
     current_user.unfollow!(params[:user_id])
     redirect_to request.referer || root_path, status: :see_other
   end
