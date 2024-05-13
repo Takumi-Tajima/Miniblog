9822085c38f36e105093cbed6e433f8bb0a19dce
taji <taji@sonicgarden.jp>
2024-05-13 13:54:44 +0900

    フォロー、フォロー解除の引数を編集

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -1,11 +1,12 @@
 class Users::RelationshipsController < ApplicationController
   def create
-    current_user.follow(params[:user_id])
+    current_user.follow(params[:follower_id])
     redirect_to request.referer || root_path
   end
 
   def destroy
-    current_user.unfollow!(params[:user_id])
+    user = Relationship.find(params[:id]).following
+    current_user.unfollow!(user.id)
     redirect_to request.referer || root_path, status: :see_other
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  has_many :relationships, foreign_key: 'following_id', dependent: :destroy, inverse_of: 'following'
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: 'followers'
-  has_many :followings, through: :relationships, source: :follower
-  has_many :followers, through: :reverse_of_relationships, source: :following
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'following_id', dependent: :destroy, inverse_of: 'following'
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: 'followers'
+  has_many :followings, through: :active_relationships, source: :follower
+  has_many :followers, through: :passive_relationships, source: :following
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/ }, length: { maximum: 20 }
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).*\z/ }
 
-  def follow(user)
-    relationships.create(follower: user)
+  def follow(user_id)
+    active_relationships.create(follower_id: user_id)
   end
 
-  def unfollow!(user)
-    relationships.find_by(follower: user).destroy!
+  def unfollow!(user_id)
+    active_relationships.find_by(following_id: user_id).destroy!
   end
 
   def following?(user)
