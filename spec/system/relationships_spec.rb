9c357dbf6e95280e66328f50d9fb91cafd247f39
taji <taji@sonicgarden.jp>
2024-05-14 16:27:05 +0900

    relationships_spec.rbのコメントアウトを削除

--- spec/system/relationships_spec.rb
+++ spec/system/relationships_spec.rb
@@ -8,41 +8,6 @@ RSpec.describe 'フォロー機能', type: :system do
     create(:post, user: other_user, content: 'こんばんは')
   end
 
-  # （別案）
-  # context 'ログインしているとき' do
-  #   before do
-  #     sign_in user
-  #   end
-
-  #   it '他のユーザーをフォローできること' do
-  #     visit root_path
-  #     expect(page).have_content 'こんばんは'
-  #     expect do
-  #       click_on 'フォロー'
-  #       # フォロー後にuser.followingが増えていることを確認する
-  #     end.to change(user.following, :count).by(1)
-  #     # フォロー後にフォローボタンが非表示になっていることを確認する
-  #     expect(page).not_to have_content 'フォロー'
-  #     # フォロー後にフォロー解除ボタンが表示さていることを確認する
-  #     expect(page).have_content 'フォロー解除'
-  #   end
-
-  #   it '他のユーザーをフォロー解除できること' do
-  #     user.follow(other_user)
-  #     visit root_path
-
-  #     expect(page).to have_content 'こんばんは'
-  #     # フォロー解除後にuser.followingが減っていることを確認する
-  #     expect do
-  #       click_on 'フォロー解除'
-  #     end.to change(user.following, :count).by(-1)
-  #     # フォロー解除後にフォロー解除ボタンが非表示になっている
-  #     expect(page).not_to have_content 'フォロー解除'
-  #     # フォロー解除後にフォローボタンが表示
-  #     expect(page).to have_content 'フォロー'
-  #   end
-  # end
-
   context '自分が他ユーザーをフォローしてないとき' do
     before do
       sign_in user
