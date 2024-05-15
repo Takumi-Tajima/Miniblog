a3811aafe77a481c0f0268bc4b6cf59476e88e40
taji <taji@sonicgarden.jp>
2024-05-16 08:19:17 +0900

    profiles_controllerを生成

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -1,11 +1,25 @@
 class Users::ProfilesController < ApplicationController
-  def show
+  before_action :set_user
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: t('controllers.common.updated', model: 'プロフィール'), status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
     @user = current_user
   end
 
-  def edit
-  end
-
-  def update
+  def user_params
+    params.require(:user).permit(:biography, :blog_url)
   end
 end
